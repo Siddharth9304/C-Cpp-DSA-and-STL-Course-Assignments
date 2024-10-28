721f69f0aa5f307617b8a6f771ad7065e7f777bf
Siddharth9304 <siddharthprem9304@gmail.com>
2024-10-28 12:53:31 +0530

    Add files via upload

---
+++ C Language Assignments/01) input and output in C Language/Assignment01.c
@@ -0,0 +1,104 @@
+//Q1. Write a Program to print Hello Students on the screen.
+/*
+int main()
+{
+    printf("Hello Students");
+    
+    return 0;
+}*/
+
+//Q2. Write a Program to print Hello in first line and Students in second line.
+/*
+int main()
+{
+    printf("Hello\nStudents");
+
+    return 0;
+}*/
+
+//Q3. Write a Program to print "My SirG" on the screen.
+/*
+int main()
+{
+    printf("\"My SirG\"");
+
+    return 0;
+}*/
+
+//Q4. Write a Program to print \n on the screen.
+/*
+int main()
+{
+    printf("\\n");
+    
+    return 0;
+}*/
+
+//Q5. Write a Program to print \r on the screen.
+/*
+int main()
+{
+    printf("\\r");
+
+    return 0;
+}*/
+
+//Q6. Write a Program to print "Teacher's Day" on the screen.
+/*
+int main()
+{
+    printf("\"Teacher's Day\"");
+
+    return 0;
+}*/
+
+//Q7. Write a Program to calculate sum of two integers.Numbers are taken from user through keyboard.
+/*
+int main()
+{
+    int a,b;
+    printf("Enter two numbers : ");
+    scanf("%d%d",&a,&b);
+    printf("Sum of given two numbers is %d",a + b);
+
+    return 0;
+}*/
+
+//Q8. Write a Program to calculate square of a given number.Number should be taken from the user.
+/*
+int main()
+{
+    int n;
+
+    printf("Enter a number : ");
+    scanf("%d",&n);
+    printf("Square of a given number is %d",n*n);
+
+    return 0;
+}*/
+
+//Q9. Write a Program to calculate area of a rectangle.Input appropriate data from the user.
+/*
+int main()
+{
+    float l,b;
+
+    printf("Enter length and breadth of a rectangle in metre respectively : ");
+    scanf("%f%f",&l,&b);
+    printf("Area of a rectangle is %f sq.m",l*b);
+
+    return 0;
+}*/
+
+//Q10. WAP to find a area of a circle.Take radius as a input from the user.
+/*
+int main()
+{
+    float r;
+
+    printf("Enter radius of a circle in metre : ");
+    scanf("%f",&r);
+    printf("Area of a circle of given radius is %f sq.m", 3.142*r*r);
+
+    return 0;
+}*/
