ee55232492e55d2ea079619119e9a80e8c42fe28
Siddharth9304 <siddharthprem9304@gmail.com>
2024-10-28 13:29:25 +0530

    Add files via upload

--- C++ Language Assignments/12) Inheritence/Assignment18.cpp
+++ C++ Language Assignments/12) Inheritence/Assignment18.cpp
@@ -1,1 +1,85 @@
+#include<iostream>
+using namespace std;
 
+
+//1.
+class Person
+{
+    private:
+        string name;
+        int age;
+    protected:
+        void setPerson(string n, int a)
+        {
+            name = n;
+            age = a;
+        }
+        string getName()
+        {
+            return name;
+        }
+        int getAge()
+        {
+            return age;
+        }
+};
+class Employee : public Person
+{
+    private:
+        int salary;
+    public:
+        void setEmployee(string n, int a, int s)
+        {
+            setPerson(n, a);
+            salary = s;
+        }
+        void showEmployee()
+        {
+            cout << "name - " << getName() << endl;
+            cout << "age - " << getAge() << endl;
+            cout << "salary - " << salary << endl;
+        }
+};
+
+
+//2.
+class Circle
+{
+    private:
+        int radius;
+    public:
+        void setRad(int r)
+        {
+            radius = r;
+        }
+        int getRadI()
+        {
+            return radius;
+        }
+        float getArea()
+        {
+            return 3.14f*radius*radius;
+        }
+};
+class ThickCircle : public Circle
+{
+    private:
+        int thickness;
+    public:
+        void setThickness(int t)
+        {
+            thickness = t;
+        }
+        int getThickness()
+        {
+            return thickness;
+        }
+};
+
+int main()
+{
+   // Employee e1;
+    //e1.setEmployee("prem", 20, 100000);
+    //e1.showEmployee();
+
+}
