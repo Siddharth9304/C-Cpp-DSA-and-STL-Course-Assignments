42206b0ec6286727f963c472b1c6df3966486537
Prem Siddhartha <siddharthprem9304@gmail.com>
2025-01-03 00:06:35 +0530

    Add files via upload

--- DSA Assignments/13) Priority Queue/Assignment13.cpp
+++ DSA Assignments/13) Priority Queue/Assignment13.cpp
@@ -1,1 +1,79 @@
+#include<iostream>
+using namespace std;
+#define UNDERFLOW 0
 
+struct node
+{
+    int item;
+    int priority;
+    node *next;
+};
+class PriorityQueue
+{
+    private:
+        node *start;
+    public:
+        PriorityQueue();
+        void insert(int, int);
+        int del();
+        int getHighestPriorityElement();
+        int getHighestPriorityNo();
+        ~PriorityQueue();
+        bool isEmpty();
+};
+PriorityQueue::PriorityQueue()
+{
+    start = nullptr;
+}
+void PriorityQueue::insert(int data, int p)
+{
+    node *n = new node;
+    n->item = data;
+    n->priority = p;
+    if(start && n->priority <= start->priority)
+    {
+        node *temp = start;
+        while(temp->next && n->priority <= temp->next->priority)
+            temp = temp->next;
+        n->next = temp->next;
+        temp->next = n;
+    }
+    else
+    {
+        n->next = start;
+        start = n;
+    }
+}
+int PriorityQueue::del()
+{
+    if(start)
+    {
+        int data = start->item;
+        node *temp = start;
+        start = start->next;
+        delete temp;
+        return data;
+    }
+    throw UNDERFLOW;
+}
+int PriorityQueue::getHighestPriorityElement()
+{
+    if(start)
+        return start->item;
+    throw UNDERFLOW;
+}
+int PriorityQueue::getHighestPriorityNo()
+{
+    if(start)
+        return start->priority;
+    throw UNDERFLOW;
+}
+PriorityQueue::~PriorityQueue()
+{
+    while(start)
+        del();
+}
+bool PriorityQueue::isEmpty()
+{
+    return !start;
+}
